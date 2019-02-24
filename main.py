e708ea0cc06ecd03b9f16fe8215bfdbd752ab7bd
RajatKatiyar <rajatkatiyar2009@gmail.com>
2019-02-24 22:32:10 +0530

    first-commit

---
+++ main.py
@@ -0,0 +1,124 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+
+# In[8]:
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+# In[9]:
+
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+# In[12]:
+
+
+batch_size = 96
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# this is a generator that will read pictures found in
+# subfolers of 'data/train', and indefinitely generate
+# batches of augmented image data
+train_generator = train_datagen.flow_from_directory(
+        'train/',  # this is the target directory
+        target_size=(150, 150),  # all images will be resized to 150x150
+        batch_size=batch_size,
+        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+# this is a similar generator, for validation data
+validation_generator = test_datagen.flow_from_directory(
+        'validation/',
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+
+# In[14]:
+
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=200,
+        epochs=25,
+        validation_data=validation_generator,
+        validation_steps=800)
+model.save('second_try.h5')
+
+
+# In[21]:
+
+
+from keras.preprocessing import image
+import numpy as np
+test_image = image.load_img('/home/katiyar/Documents/ML/Cat03.jpg',target_size=(150,150))
+test_image.show()
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+result = model.predict(test_image)
+if(result[0,0]<0.5):
+    print("CAT",result[0,0])
+else:
+    print("DOG",result[0,0])
+
